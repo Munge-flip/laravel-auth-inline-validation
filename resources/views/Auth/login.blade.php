1df2e0f11357ca1b8093fb40d5b535ae777350bf
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-13 10:40:06 +0800

    commit

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,3 +1,26 @@
-Login
+<form method="POST" action=" {{ route('login') }} ">
 
-<a href=" {{ route('register') }} ">Register</a>
+    @csrf
+
+    <h3>Login</h3>
+
+    <div>
+        <label for="">Email</label><br>
+        <input type="email" name="email" value=" {{ old('email') }} " required><br>
+        @error('email')
+        <div style="color: red"> {{ $message }} </div>
+        @enderror
+    </div>
+
+    <div>
+        <label for="">Password</label><br>
+        <input type="password" name="passwword" required><br>
+        @error('password')
+        <div style="color: red"> {{ $message }} </div>
+        @enderror
+    </div>
+
+    <button>Login</button>
+
+    <a href=" {{ route('register') }} ">Don't have an account? Register</a>
+</form>
--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -1,3 +1,37 @@
-Register
+<form method="POST" action="">
 
-<a href=" {{ route('login') }} ">Already have an Account?</a>
+    <h3>Register</h3>
+
+    <div>
+        <label for="">Name</label><br>
+        <input type="text" name="name" value=" {{old('name')}} " required><br>
+        @error('name')
+        <div style="color: red"> {{ $message }} </div>
+        @enderror
+    </div>
+
+    <div>
+        <label for="">Email</label><br>
+        <input type="email" name="email" value=" {{old('email')}} " required><br>
+        @error('email')
+        <div style="color: red"> {{ $message }} </div>
+        @enderror
+    </div>
+
+    <div>
+        <label for="">Password</label><br>
+        <input type="password" name="passwword" required><br>
+        @error('password')
+            <div style="color: red"> {{ $message }} </div>
+        @enderror
+    </div>
+
+    <div>
+    <label for="">Confirm Password</label>
+    <input type="password" name="password_confirmation" required>
+    </div>
+
+    <button>Register</button>
+
+    <a href=" {{ route('login') }} ">Already have an Account?</a>
+</form>
