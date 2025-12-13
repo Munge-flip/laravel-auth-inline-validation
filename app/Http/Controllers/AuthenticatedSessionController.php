829c7cafd60dea8d2c8641735029593d9070b61d
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-13 11:39:57 +0800

    commit

--- app/Http/Controllers/AuthenticatedSessionController.php
+++ app/Http/Controllers/AuthenticatedSessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthenticatedSessionController extends Controller
@@ -9,7 +10,23 @@ class AuthenticatedSessionController extends Controller
     public function create() {
         return view ('Auth.login');
     }
-    public function store() {
-        
+    public function store(Request $request) {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended(route('form.index'));
+        }
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records',
+        ]);
+    }
+    public function destroy(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('login');
     }
 }
--- resources/views/Forms/index.blade.php
+++ resources/views/Forms/index.blade.php
@@ -1,3 +1,8 @@
 this is the index
 
 <a href=" {{ route('form.create') }} " class="btn-primary">Add</a>
+
+<form method="POST" action=" {{ route('logout') }} ">
+    @csrf
+    <button type="submit">Logout</button>
+</form>
