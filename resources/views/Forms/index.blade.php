7886aad6a8985bb70c43f63c78050c84684f54c2
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-17 22:23:48 +0800

    commit

--- resources/views/Forms/index.blade.php
+++ resources/views/Forms/index.blade.php
@@ -11,12 +11,16 @@ this is the index
         <th>ID</th>
         <th>Username</th>
         <th>Date Created</th>
+        <th>Action</th>
     </thead>
     @forelse($forms as $form)
     <tr>
         <td> {{$form->id}} </td>
         <td> {{$form->username}} </td>
         <td> {{$form->created_at->format('M d, Y')}} </td>
+        <td>
+            <a href="{{route('forms.edit', $form->id)}}">Edit</a>
+        </td>
     </tr>
     @empty
     <tr>
