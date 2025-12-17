a988be8410c97bb8b215ecf9a0921bb20f375eb7
Munge-flip <ggonzales2@ssct.edu.ph>
2025-12-17 21:55:09 +0800

    commit

--- app/Http/Controllers/AuthenticatedSessionController.php
+++ app/Http/Controllers/AuthenticatedSessionController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
 {
     public function create()
     {
         return view('auth.login');
+    }
+    public function store() {
+        
+    }
+    public function destroy(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login');
     }
 }
